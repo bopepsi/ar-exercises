98ddb1a520b0ea6f265fffcd4769aa634fc53925
Bowen <bopepsi@gmail.com>
2022-06-27 20:33:05 +0000

    ex2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id:1)
+puts @store1.inspect
+
+@store2 = Store.find_by(id:2)
+@store1.name = 'Lego'
+@store1.save
+puts @store1.inspect
+
